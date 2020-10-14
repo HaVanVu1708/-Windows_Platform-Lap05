3a973d0c7cb0a029455c0a15d1038ac3ad9900f9
ha van vu <62437220+HaVanVu1708@users.noreply.github.com>
2020-10-15 01:23:04 +0700

    .

---
+++ HamburgerHeavenChallenge1/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HamburgerHeavenChallenge1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MyFrame.Navigate(typeof(Home));
+            TitleTextBlock.Text = "Home";
+            Home.IsSelected = true;
+            
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Home.IsSelected)
+            {
+                MyFrame.Navigate(typeof(Home));
+                TitleTextBlock.Text = "Home";
+            }
+            else if(Markets.IsSelected)
+            {
+                MyFrame.Navigate(typeof(Markets));
+                TitleTextBlock.Text = "Markets";
+            }
+            if(Currencies.IsSelected)
+            {
+                MyFrame.Navigate(typeof(Currencies));
+                TitleTextBlock.Text = "Currencies";
+            }
+        }
+    }
+}
